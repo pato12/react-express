687de92e26adfae3adef09ae014e90776b4712bf
Pato Perez <patoperez@MacBook-Pro-de-Pato.local>
2019-10-20 20:54:10 -0300

    Add types

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,67 @@
 import Renderer from '@root/renderer';
 import { Elements, Methods } from '@root/types';
+import * as express from 'express';
+import { ReactNode } from 'react';
 
-const Express = Elements.Express as any;
-const Middleware = Elements.Middleware as any;
-const Root = Elements.Root as any;
-const Route = Elements.Route as any;
-const ErrorHandler = Elements.ErrorHandler as any;
+export type PathProp = string | RegExp | Array<string | RegExp>;
+
+interface BaseComponent {
+  children?: ReactNode | ReactNode[];
+}
+
+type RouteHandle = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => any;
+
+type ErrorHandle = (
+  err: any,
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => any;
+
+interface IMiddlewareComponentProps extends BaseComponent {
+  path: PathProp;
+  handle: Partial<RouteHandle>;
+}
+
+type IMiddlewareComponent = (
+  props: Partial<IMiddlewareComponentProps>
+) => JSX.Element;
+
+interface IExpressComponentProps extends BaseComponent {
+  path: PathProp;
+}
+
+type IExpressComponent = (
+  props: Partial<IExpressComponentProps>
+) => JSX.Element;
+
+interface IErrorHandlerComponentProp extends BaseComponent {
+  handle: ErrorHandle;
+}
+
+type IErrorHandlerComponent = (
+  props: IErrorHandlerComponentProp
+) => JSX.Element;
+
+interface IRouteComponentProps extends BaseComponent {
+  path: PathProp;
+  method: string;
+  handle: Partial<RouteHandle>;
+}
+
+type IRouteComponent = (props: Partial<IRouteComponentProps>) => JSX.Element;
+
+const Express: IExpressComponent = Elements.Express as any;
+const Middleware: IMiddlewareComponent = Elements.Middleware as any;
+const Route: IRouteComponent = Elements.Route as any;
+const ErrorHandler: IErrorHandlerComponent = Elements.ErrorHandler as any;
 
 function compile(element) {
   return Renderer.compile(Renderer.generate(element));
 }
 
-export {
-  Renderer,
-  Express,
-  Middleware,
-  Root,
-  Route,
-  ErrorHandler,
-  Methods,
-  compile,
-};
+export { Renderer, Express, Middleware, Route, ErrorHandler, Methods, compile };
